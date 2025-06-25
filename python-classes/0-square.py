179c77a1797cbef380ec7c8e58466c7f9d1ad40e
narmingasimli <nerminqasimli210@gmail.com>
2025-06-25 12:56:18 +0000

    update 0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 class Square:
+    """
+    Defines a square.
+
+    Attributes:
+        __size (int): The size of the square (private attribute).
+    """
+
     def __init__(self, size):
         self.__size = size
